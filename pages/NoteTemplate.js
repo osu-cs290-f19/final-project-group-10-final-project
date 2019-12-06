38e69e1bf2519ab97adc1087e89d43b5cb05cc09
Michael Hathaway <michaelhathaway@10-249-116-126.wireless.oregonstate.edu>
2019-12-06 14:05:05 -0800

    Added directories for templates, added NoteData.json, added potential Note handlebars template.

---
+++ pages/NoteTemplate.js
@@ -0,0 +1,20 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['NoteTemplate'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "/*Potential Layout for Note*/\n\n<div class=\"Note\" data-course=\""
+    + alias4(((helper = (helper = helpers.course || (depth0 != null ? depth0.course : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"course","hash":{},"data":data,"loc":{"start":{"line":3,"column":31},"end":{"line":3,"column":41}}}) : helper)))
+    + "\" data-title=\""
+    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":3,"column":55},"end":{"line":3,"column":64}}}) : helper)))
+    + "\" data-created=\""
+    + alias4(((helper = (helper = helpers.created || (depth0 != null ? depth0.created : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"created","hash":{},"data":data,"loc":{"start":{"line":3,"column":80},"end":{"line":3,"column":91}}}) : helper)))
+    + "\" data-modified=\""
+    + alias4(((helper = (helper = helpers.modified || (depth0 != null ? depth0.modified : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"modified","hash":{},"data":data,"loc":{"start":{"line":3,"column":108},"end":{"line":3,"column":120}}}) : helper)))
+    + "\">\n  <div class=\"Note-Title-Container\">\n    <h3> "
+    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":5,"column":9},"end":{"line":5,"column":18}}}) : helper)))
+    + " </h3>\n  </div>\n  <div class=\"Note-Content-Container\">\n    <p> "
+    + alias4(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"content","hash":{},"data":data,"loc":{"start":{"line":8,"column":8},"end":{"line":8,"column":19}}}) : helper)))
+    + " </p>\n  </div>\n</div>\n";
+},"useData":true});
+})();
--- server.js
+++ server.js
@@ -5,11 +5,24 @@ Description: server implementation using express.js for Oregon State CS 290 Fina
 */
 
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-//use object to cache pages, so don't have to read from disk for every request
-var serverCache = {};
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//use JSON Parser
+app.use(bodyParser.json());
+
+//use the public directory
+app.use(express.static('pages'));
+
+//load data from JSON File
+var NoteData = require("./NoteData.json")
 
 //logger function
 function logger(req, res, next){
@@ -19,24 +32,10 @@ function logger(req, res, next){
 
 	next();
 }
-
-//add logger to app 
+//add logger to app
 app.use(logger);
 
 //Set server to listen at port specified by the port variable(currently 3000)
 app.listen(port, function(){
 	console.log('The server is listening at port', port);
-});
-
-//Handle get request for '/'
-app.get('/', function(req, res, next){
-	res.status(200).sendFile(__dirname + '/pages/index.html');
-});
-
-
-app.use(express.static('pages'));
-
-//handle requests for pages that do not exist
-app.get('*', function(req, res, next){
-	res.status(404).sendFile(__dirname + '/pages/404.html');
 });
