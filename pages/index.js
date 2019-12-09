f3d1877a2c360f02dbf8508ec76a266481f973d8
abbyylam <56245965+abbyylam@users.noreply.github.com>
2019-12-09 04:45:16 -0800

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
 /*NEW NOTE MODAL*/
 // Get the modal
 var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
 
 // Get the button that opens the modal
 var btn = document.getElementById("add-note");
+var btn2 = document.getElementById("add-class");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+}
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -24,6 +31,7 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
 
 
 
